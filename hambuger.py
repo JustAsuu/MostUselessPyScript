b0e5f94b58b05e3386b6383a17b15e36c688d4f6
JustAsuu <88696849+JustAsuu@users.noreply.github.com>
2021-08-10 10:17:04 +0700

    i think it works??

---
+++ hambuger.py
@@ -0,0 +1,19 @@
+note: i didnt even test this i was bored and wrote it in my browser
+
+wantednumb = 100
+
+-- da code
+
+currentnum = 1
+Isthereyet = False
+
+while Isthereyet == false:
+  local newnumb = randint(1, wantednumb * 1.5)
+  if currentnumb == wantednumb:
+    print("Woah it actually happened and you just ran this and wasted time of your life gg")
+  elseif currentnum - newnum < 0:
+    print("too less")
+    currentnum += newnum
+  elseif currentnumb + newnumb > wantednumb * 2:
+    print("Too much")
+    currentnum += -newnumb
